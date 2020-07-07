a41322b27edc8d9cc6352da2bf9e158dc3ecf347
sah_manish <manish.sah@debajyotigroup.com>
2020-07-07 20:59:34 +0545

    use defer to solve blocking scripts problem

--- javascript/basics/what is javascript/static/js/index.js
+++ javascript/basics/what is javascript/static/js/index.js
@@ -6,3 +6,22 @@ function updateName(){
     let name = prompt('Enter a new name');
     para.textContent = 'Player 1: ' + name;
 }
+
+// select all the elements with tag button
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+    buttons[i].addEventListener('click', createParagraph);
+  }
+
+function createParagraph() {
+    let para = document.createElement('p');
+    para.textContent = 'You clicked the button!';
+    document.body.appendChild(para);
+  }
+
+function createParagraph2(){
+    let para = document.createElement('p')
+    para.textContent = 'You click paragraph 2';
+    document.body.appendChild(para);
+}
